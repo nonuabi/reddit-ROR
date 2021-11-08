b46e0968025896ee39c30fa927eca1aad652b0d3
unknown <nonuabi@gmail.com>
2021-11-08 22:24:25 +0530

    remove cerdi

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -2,6 +2,6 @@ class NotifierMailer < ApplicationMailer
     def alert_admin(post)
         @title = post.title
         @body = post.body
-        mail(:to=>"pro.albus.dumbledore@gmail.com", :subject=> "Alert -> A new post is added")
+        mail(:to=>"testing@testing.com", :subject=> "Alert -> A new post is added")
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,13 +60,5 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            'nonuabii@gmail.com',
-    password:             'Nonuabi<98>',
-    authentication:       'plain',
-    enable_starttls_auto: true }
+  
 end
