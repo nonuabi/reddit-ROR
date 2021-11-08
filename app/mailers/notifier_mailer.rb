4074d85fbacfe3fc796a208c730bbd7b5c4110c8
Abhishke Verma <nonuabi@gmail.com>
2021-11-08 09:34:15 +0530

    added stmp config

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,2 +1,7 @@
 class NotifierMailer < ApplicationMailer
+    def alert_admin(post)
+        @title = post.title
+        @body = post.body
+        mail(:to=>"pro.albus.dumbledore@gmail.com", :subject=> "Alert -> A new post is added")
+    end
 end
