0972da3ba88236f4fd6d686f53942a29ac1eb652
unknown <nonuabi@gmail.com>
2021-11-09 10:57:09 +0530

    changed the email id

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -2,6 +2,6 @@ class NotifierMailer < ApplicationMailer
     def alert_admin(post)
         @title = post.title
         @body = post.body
-        mail(:to=>"testing@testing.com", :subject=> "Alert -> A new post is added")
+        mail(:to=>"testing@example.com", :subject=> "Alert -> A new post is added")
     end
 end
